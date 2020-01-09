f875593b237dc5875e924afb4dfce880052433a5
rakimdevcraig <rakimdevcraig@gmail.com>
2020-01-09 13:02:26 -0500

    test2

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
 const { exec } = require('child_process');
 
-exec('git add .', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(`git add command ran`)
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-  });
+function runTerminalCommand(command){
+    exec(command, (error) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(`git add command ran`)
+      });
+
+}
+
+runTerminalCommand('git add .')
+runTerminalCommand('git commit -m test2')
+runTerminalCommand('git push')
+
+
+
+
+
+// exec('git add .', (error) => {
+//     if (error) {
+//       console.error(`exec error: ${error}`);
+//       return;
+//     }
+//     console.log(`git add command ran`)
+//   });
   
-exec('git commit -m test', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(`git commit command ran`)
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-  });
+// exec('git commit -m test', (error) => {
+//     if (error) {
+//       console.error(`exec error: ${error}`);
+//       return;
+//     }
+//     console.log(`git commit command ran`)
+//   });
   
-  exec('git push', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(`git commit command ran`)
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-  });
+//   exec('git push', (error) => {
+//     if (error) {
+//       console.error(`exec error: ${error}`);
+//       return;
+//     }
+//     console.log(`git push command ran`)
+//   });

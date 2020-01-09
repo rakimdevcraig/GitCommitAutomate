363705798f8bf4ae8227aac312b4d34be08470fe
rakimdevcraig <rakimdevcraig@gmail.com>
2020-01-09 12:52:59 -0500

    test

---
+++ index.js
@@ -0,0 +1,22 @@
+const { exec } = require('child_process');
+
+exec('git add .', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`git add command ran`)
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
+  
+exec('git commit -m test', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`git commit command ran`)
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
+  

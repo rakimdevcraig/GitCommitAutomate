c54520b5c9df47dac2a9397302c7a5bdd6cecf79
rakimdevcraig <rakimdevcraig@gmail.com>
2020-01-09 12:53:29 -0500

    test

--- index.js
+++ index.js
@@ -20,3 +20,12 @@ exec('git commit -m test', (error, stdout, stderr) => {
     console.error(`stderr: ${stderr}`);
   });
   
+  exec('git push', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`git commit command ran`)
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
